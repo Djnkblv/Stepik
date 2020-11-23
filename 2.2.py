0309842252478f2daf3e9a20f122618ed373198a
MiK <djnkblv@gmail.com>
2020-11-23 21:08:48 +0700

    final lesson 2.2

--- 2.2.py
+++ 2.2.py
@@ -2,6 +2,7 @@ from idlelib import browser
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
+import math
 
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
@@ -9,12 +10,13 @@ driver.implicitly_wait(5)
 driver.get('http://suninjuly.github.io/selects1.html')
 
 x_select = driver.find_element_by_css_selector('#num1')
-int(x) = x_select.text
+x = x_select.text
 y_select = driver.find_element_by_css_selector('#num2')
-int(y) = y_select.text
-int(otvet) = x + y
+y = y_select.text
+otvet = (int(x)+int(y))
 
-
-select = Select(browser.find_element_by_tag_name("select"))
+dropdown = driver.find_element_by_css_selector('#dropdown')
+dropdown.click()
+select = Select(driver.find_element_by_tag_name("select"))
 select.select_by_value(otvet)
 select.click()
