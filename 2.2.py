2a44617d84938f203498d152f13fc1fbd6f775f0
MiK <djnkblv@gmail.com>
2020-11-23 20:06:23 +0700

    final 2.2

--- 2.2.py
+++ 2.2.py
@@ -1,20 +1,20 @@
-#from idlelib import browser
-#from selenium import webdriver
-#from selenium.webdriver.common.action_chains import ActionChains
-#from selenium.webdriver.support.ui import Select
+from idlelib import browser
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
 
 
-#driver = webdriver.Chrome(executable_path='chromedriver.exe')
-#driver.implicitly_wait(5)
-#driver.get('http://suninjuly.github.io/selects1.html')
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+driver.implicitly_wait(5)
+driver.get('http://suninjuly.github.io/selects1.html')
 
-#x_select = driver.find_element_by_css_selector('#num1')
-#int(x) = x_select.text
-#y_select = driver.find_element_by_css_selector('#num2')
-#int(y) = y_select.text
-#int(otvet) = x + y
+x_select = driver.find_element_by_css_selector('#num1')
+int(x) = x_select.text
+y_select = driver.find_element_by_css_selector('#num2')
+int(y) = y_select.text
+int(otvet) = x + y
 
 
-#select = Select(browser.find_element_by_tag_name("select"))
-#select.select_by_value(otvet)
-#select.click()
+select = Select(browser.find_element_by_tag_name("select"))
+select.select_by_value(otvet)
+select.click()
